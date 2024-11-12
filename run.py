357db209b66c62c99b462a14c1ea9bae75c82f7f
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-11-12 00:44:56 -0600

    basics update

---
+++ run.py
@@ -0,0 +1,8 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == 'main':
+    # Update paths if necessary
+    context = ('certs/cert.pem', 'certs/key.pem')
+    app.run(host = '0.0.0.0', port = 443, ssl_context = context)
